55e91bfe341eb6c72d30c677e5b318aff4c575ef
Martazanov <saidmartazanov1@gmail.com>
2022-12-03 21:19:01 +0300

    minimal-server

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const { PORT, MONGO_DB } = process.env;
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+mongoose
+  .connect(MONGO_DB)
+  .then(() => {
+    console.log("Mongo connected");
+  })
+  .catch((e) => e.toString());
+
+app.listen(PORT, () => {
+  console.log(`Server start htpp://localhost:${PORT}`);
+});

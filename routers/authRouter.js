22a495a5c4d3a4836e15019c05ab872ac4622baa
John Doe <johndoe@example.com>
2022-12-05 01:04:53 +0300

    завершил авторизацию на backend

---
+++ routers/authRouter.js
@@ -0,0 +1,14 @@
+const Router = require('express')
+const router = Router()
+const {authController} = require('../controllers/authController')
+const {check} = require('express-validator')
+const authMiddleware = require('../middlewares/auth.middleware')
+
+router.post('/registration',[
+    check('login','логин не может быть пустым').notEmpty(),
+    check('password','пароль должен быть не короче 4 и не больше 10 символов').isLength({max:10,min:4})
+], authController.registration)
+router.post('/login',authController.login)
+router.get('/users', authMiddleware, authController.users)
+
+module.exports = router
